834532ebbcf9e3f987676202558babcc3d2e54e1
DJ211285 <dj211285@reigate.ac.uk>
2021-10-20 10:59:41 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        String [] [] board = new String [5][5];
+        board[0][0] = Integer.toString(random.nextInt(5));
+        System.out.println(board[0][0] + " coins!");
+
+        System.out.println("Enter your coordinates");
+        int Usernput = input.nextInt();
+        
+
+
+//      int[] treasureCords = new int[];
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                board[i][j] = board[0][0];
+//                System.out.print(board[i][j]);
+//            }
+//            System.out.println("");
+        }
+
+
+
+
+
+
+
+
+//	Random random =  new Random();
+//    int[] favouriteNumbers = new int[10];
+//
+//    for (int i = 1; i < favouriteNumbers.length; i++) {
+//        favouriteNumbers[i] = random.nextInt(500);
+//        System.out.println(favouriteNumbers[i]);
+//
+//
+//        }
+//        char[] vowels = {'u', 'o', 'i', 'e', 'a'};
+//        char[] consonants = new char[21];
+//        int newChar = 0;
+//        boolean found;
+//        for (char i = 'a'; i <= 'z'; i++) {
+//            for (int j = 0; j < vowels.length; j++) {
+//                break;
+//            }
+//            if (!found) {
+//                consonants[newChar] = i;
+//                newChar++;
+//            }
+//        }
+//        Arrays.sort(vowels);
+//        System.out.println(Arrays.toString(vowels));
+//        System.out.println(Arrays.toString(consonants));
+
+//        String [] [] board = new String [10][8];
+//
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 8; j++) {
+//                board[i][j] = "[ ]";
+//                System.out.print(board[i][j]);
+//            }
+//            System.out.println("");
+        }
+
+
+
